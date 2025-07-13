8f8611992f190793500a486cd94b695fa8e2d9cd
Aleksandr <shaban4@yandex.ru>
2025-07-13 23:03:07 +0700

    add popup and bugfix

--- dist/js/app.js
+++ dist/js/app.js
@@ -3777,19 +3777,6 @@
         "use strict";
         const modules_flsModules = {};
         window.flsModules = modules_flsModules;
-        function isWebp() {
-            function testWebP(callback) {
-                let webP = new Image;
-                webP.onload = webP.onerror = function() {
-                    callback(webP.height == 2);
-                };
-                webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-            }
-            testWebP(function(support) {
-                let className = support === true ? "webp" : "no-webp";
-                document.documentElement.classList.add(className);
-            });
-        }
         let isMobile = {
             Android: function() {
                 return navigator.userAgent.match(/Android/i);
@@ -5039,7 +5026,6 @@
         }
         if (scrollabelBlock) requestAnimationFrame(updatePosition);
         window["FLS"] = true;
-        isWebp();
         menuInit();
         formFieldsInit({
             viewPass: true,
